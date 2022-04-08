fa19aaf2cea72a2002b3ae18a9b10ccc86d50e29
bgua <qbrian2009@yahoo.com>
2022-04-08 14:12:11 +0800

    add few constants

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const BASE_URL = "http://codingcase.zeiss.services"
+export const WS_BASE_URL = "ws://codingcase.zeiss.services"
+export const MACHINES_URL = "/api/v1/machines"
+export const MACHINES_DETAIL_URL = "/api/v1/machines"
+export const WS_ENDPOINT = "/ws"
